ed520517d4f7754ff5526221d313c7e9ad619dae
meinalisaa <meinalisa02@gmail.com>
2023-10-02 10:55:51 +0700

    elemen login (test)

---
+++ app/Views/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Login RumahDev Kasir">
+  <meta name="keywords" content="rumahdev, kasir">
+  <meta name="author" content="RumahDev">
+  <title>Login</title>
+</head>
+
+<body>
+  <header>
+    <h1>Login RumahDev Kasir</h1>
+  </header>
+  <main>
+    <section>
+      <h2>Masuk ke Akun</h2>
+      <p>Username : <input type="text" name="username"></p>
+      <p>Password : <input type="password" name="password"></p>
+    </section>
+    <section>
+      <button>Login</button>
+    </section>
+  </main>
+  <footer>
+    <p>&copy; 2023 RumahDev</p>
+  </footer>
+</body>
+</html>
+
+<style type="text/css">
+header, main, footer {
+  text-align: center;
+}
+
+</style>
